480fd93f762fd34c5f41699c84efe9d71a1a59eb
makarovos <makaroff297991@gmail.com>
2019-02-26 23:38:54 +0300

    Add new files and limitations

---
+++ Back/Clinic/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Clinic.Controllers
+{
+    using Clinic.Domain;
+    using Clinic.Support.Filters;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    [UserPermission(UserPermission.All)]
+    [Route("users")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+    }
+}
