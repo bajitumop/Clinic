e77cecc88a076c5dafadc6b333e78f6764837b79
makarovos <makaroff297991@gmail.com>
2019-06-12 16:25:44 +0300

    Front: Change locale to russian

--- Front/app/src/main/java/bajitumop/clinic/ClinicApplication.java
+++ Front/app/src/main/java/bajitumop/clinic/ClinicApplication.java
@@ -1,6 +1,9 @@
 package bajitumop.clinic;
 
 import android.app.Application;
+import android.content.res.Configuration;
+
+import java.util.Locale;
 
 import bajitumop.clinic.services.network.UserStorage;
 
@@ -15,5 +18,10 @@ public class ClinicApplication extends Application {
     public void onCreate() {
         super.onCreate();
         userStorage = UserStorage.Create(this);
+        Locale locale = new Locale("ru","RU");
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getApplicationContext().getResources().updateConfiguration(config, null);
     }
 }
